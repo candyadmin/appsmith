c4e0da7f4f7869858c53b08e16678b313b30a7e3
BaseAccept <193661405@qq.com>
2023-03-11 15:27:14 +0800

    尝试保存ApplicationTemplate模板

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ApplicationTemplateDTO.java
@@ -0,0 +1,13 @@
+package com.appsmith.server.dtos;
+
+import com.appsmith.external.models.BaseDomain;
+import com.appsmith.server.domains.ApplicationTemplate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ApplicationTemplateDTO extends ApplicationTemplate {
+}
